bd20f125420f6bed3c9332cc35b9561b8e04f5cf
Skyfi-admin <lakshmanachandran@gmail.com>
2026-02-20 23:35:48 +0530

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 10000;
+const MONGO_URL = process.env.MONGO_URL;
+
+// MongoDB connect
+mongoose.connect(MONGO_URL)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("SkyFi Server Running 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
