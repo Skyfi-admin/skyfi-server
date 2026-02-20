dbac020e57b540813fa86b6a18c71500d4c1a1d4
Skyfi-admin <lakshmanachandran@gmail.com>
2026-02-20 23:44:47 +0530

    server.js

--- server.js
+++ server.js
@@ -1,4 +1,30 @@
+const CustomerSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  plan: String,
+  usage: { type: Number, default: 0 }
+});
+app.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
+    const user = await Customer.findOne({ username, password });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    res.json({
+      message: "Login success",
+      customer: user
+    });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+const Customer = mongoose.model("Customer", CustomerSchema);
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
